84c22f4fb54ffc35ba891bfc26d3244caf745d96
Dan Byström <dan.bystrom@gmail.com>
2024-07-18 00:18:36 +0200

    remove use of entrypoint

--- src/index.js
+++ src/index.js
@@ -60,44 +60,33 @@ export const checkForNewerVersions = async () => {
         .filter(file => file.startsWith('versions/') && getVersion(file))
         .map(file => getVersion(file));
 
-    // Get the basename of the file
-    const basename = path.basename(entrypoint);
-    // Get the parent directory's full path
-    const parentDirPath = path.dirname(entrypoint);
-    // Get the parent directory's name
-    const parentDirName = path.basename(parentDirPath);
     let newerFiles
     // If new versions are available, download them from S3
-    if(parentDirName=='versions') {
-        const baseVersion = pkg.version;
-        const newerVersions = versions
-            .filter(version => isVersionNewer(version, baseVersion));
-        newerFiles = files.filter(file => newerVersions.includes(getVersion(file)));
-        for (const file of newerFiles) {
+    const baseVersion = pkg.version;
+    const newerVersions = versions
+        .filter(version => isVersionNewer(version, baseVersion));
+    newerFiles = files.filter(file => newerVersions.includes(getVersion(file)));
+    for (const file of newerFiles) {
+        try {
+            // Check if the file exists
+            await fs.access(filePath);
+        } catch (err1) {
+            // If the file does not exist, error is thrown, then upload the file
+            const bufferData = await download(file);
             try {
-                // Check if the file exists
-                await fs.access(filePath);
-            } catch (err1) {
-                // If the file does not exist, error is thrown, then upload the file
-                const bufferData = await download(file);
-                try {
-                    await fs.writeFile(file, bufferData);
-                    info(`Downloaded ${file} successfully`);
-                } catch (err2) {
-                    newerFiles = newerFiles.filter(f => f !== file);
-                    error(`Failed writing ${file}: ${err2}`);
-                }
+                await fs.writeFile(file, bufferData);
+                info(`Downloaded ${file} successfully`);
+            } catch (err2) {
+                newerFiles = newerFiles.filter(f => f !== file);
+                error(`Failed writing ${file}: ${err2}`);
             }
         }
     }
 
     // Log if there are newer versions on disk and give instructions how to update winsw service with the latest version
     if (newerFiles.length > 0) {
-        const workingDirectory = path.dirname(parentDirPath);
-        // Get the parent directory's name
-        const workingDirectorytDirName = path.basename(workingDirectory);
         info(`There are newer versions available: ${newerFiles.join(', ')}`);
-        info(`To run a newer version, edit ${workingDirectorytDirName}.xml and update the version number in the <executable> tag and restart the service.`);
+        info(`To run a newer version, edit ${pkg.name}.xml and update the version number in the <executable> tag and restart the service.`);
     }
 }
 
